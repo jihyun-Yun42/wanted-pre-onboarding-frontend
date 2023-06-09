69563ea5249391cd91be985d237d998e27624832
jihyun Yun <jyun9842@gmail.com>
2023-06-09 11:24:27 +0900

    [feat] CREATE, READ 구현 #5

---
+++ src/pages/todo/CreateTodo.jsx
@@ -0,0 +1,31 @@
+import React, { useState } from "react";
+import api from "../api/axios";
+
+const CreateTodo = () => {
+  const [todo, setTodo] = useState("");
+
+  const changeInputHandler = (e) => {
+    setTodo(e.target.value);
+  };
+
+  const submitFormHandler = async (e) => {
+    e.preventDefault();
+    try {
+      await api.post("/todos", { todo });
+      setTodo("");
+    } catch {
+      alert("잠시 후 다시 시도해주세요");
+    }
+  };
+
+  return (
+    <form onSubmit={submitFormHandler}>
+      <input data-testid="new-todo-input" value={todo} onChange={changeInputHandler} />
+      <button data-testid="new-todo-add-button" type="submit">
+        추가
+      </button>
+    </form>
+  );
+};
+
+export default CreateTodo;
---
+++ src/pages/todo/Todo.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import CreateTodo from "./CreateTodo";
+
+const Todo = () => {
+  return (
+    <>
+      <CreateTodo />
+    </>
+  );
+};
+
+export default Todo;
