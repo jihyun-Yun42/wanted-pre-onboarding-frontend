be5ff38da0854e5d36142d05a7c804574caaf5a8
jihyun Yun <jyun9842@gmail.com>
2023-06-09 13:51:36 +0900

    [feat] 로그인 유무에따라 home UI변경

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,12 +3,20 @@ import { useNavigate } from "react-router-dom";
 
 const Home = () => {
   const navigate = useNavigate();
+  const token = localStorage.getItem("access_token");
 
   return (
     <>
-      <button onClick={() => navigate("/todo")}>투두</button>
-      <button onClick={() => navigate("/signin")}>로그인</button>
-      <button onClick={() => navigate("/signup")}>회원가입</button>
+      {token ? (
+        <button onClick={() => navigate("/todo")}>투두</button>
+      ) : (
+        <>
+          <span>로그인 먼저 해주세요</span>
+          <br />
+          <button onClick={() => navigate("/signin")}>로그인</button>
+          <button onClick={() => navigate("/signup")}>회원가입</button>
+        </>
+      )}
     </>
   );
 };
